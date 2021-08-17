a20822ff65134038d589c47cd35f623f7de8cf7f
pwl777 <68010303+pwl777@users.noreply.github.com>
2021-08-17 17:19:17 +0100

    Added dynamic date to footer

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* ------ Javascript - Founders & Coders - Personal Website ------ */
+
+/* =============== Dynamically Set Copyright Date =============== */
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
