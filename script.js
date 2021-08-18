3f46573b18af134797033bc476edadef39d6539d
pwl777 <68010303+pwl777@users.noreply.github.com>
2021-08-18 09:41:19 +0100

    Included dark to light toggle

--- script.js
+++ script.js
@@ -81,3 +81,58 @@ scrollLinks.forEach(function (link) {
     linksContainer.style.height = 0;
   });
 });
+
+/* ------ Javascript - Light & Dark Mode ------ */
+
+/* =============== Target Light Mode Switch =============== */
+
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+/* =============== Target Other Elements =============== */
+
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+
+/* =============== Toggle Dark / Light Mode =============== */
+
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 60%)"
+    : "rgb(255 255 255 / 60%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+}
+
+/* =============== Dynamically Switch Themes =============== */
+
+function switchTheme(event) {
+  //   console.log(event.target.checked);
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    toggleDarkLightMode(true);
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    toggleDarkLightMode(false);
+  }
+}
+
+/* =============== Event Listener using a change event =============== */
+
+toggleSwitch.addEventListener("change", switchTheme);
+
+/* =============== Check Local Storage For Theme =============== */
+
+const currentTheme = localStorage.getItem("theme");
+// console.log(currentTheme);
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
